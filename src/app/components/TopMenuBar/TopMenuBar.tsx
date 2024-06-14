54a1c782297cad05007cbf93115daca64efc6f32
zbqmal <zbqmal@gmail.com>
2024-06-13 22:50:23 -0400

    feat(RN-005): adds accountIcon and dummy tabs on bottom

--- src/app/components/TopMenuBar/TopMenuBar.tsx
+++ src/app/components/TopMenuBar/TopMenuBar.tsx
@@ -1,15 +1,38 @@
+"use client";
+
 import {
   AppBar,
   Box,
-  Button,
   IconButton,
+  Menu,
+  MenuItem,
   Toolbar,
   Typography,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import React from "react";
+import React, { useState } from "react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { accountIconStyle } from "./styles";
 
 export const TopMenuBar = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const id = open ? "account-menu" : undefined;
+
+  // Handlers
+  const handleAccountClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleMenuItemClick = (option: string) => {
+    console.log(`${option} clicked`);
+    handleClose();
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -26,9 +49,30 @@ export const TopMenuBar = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Rolling Novel
           </Typography>
-          <Button color="inherit">Login</Button>
+          <IconButton onClick={handleAccountClick}>
+            <AccountCircleIcon sx={accountIconStyle} />
+          </IconButton>
         </Toolbar>
       </AppBar>
+      <Menu
+        id={id}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <MenuItem onClick={() => handleMenuItemClick("Test")}>Test</MenuItem>
+        <MenuItem onClick={() => handleMenuItemClick("Sign In")}>
+          Sign In
+        </MenuItem>
+      </Menu>
     </Box>
   );
 };
